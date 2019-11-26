191ce7d171e3adbcdd0b0c50acf401e89b0f1de5
Jung Chan <jungchan2013@gmail.com>
2019-11-26 21:16:56 +0800

    implemented serializer

--- src/io/Serializer.java
+++ src/io/Serializer.java
@@ -1,5 +1,6 @@
 package io;
 
+import models.MapElement;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedWriter;
@@ -29,6 +30,25 @@ public class Serializer {
      * @throws IOException if an I/O exception has occurred.
      */
     public void serializeGameProp(@NotNull final GameProperties prop) throws IOException {
-        // TODO
+        // TODO done
+        try{
+            PrintWriter pw = new PrintWriter(path.toFile());
+            pw.println(prop.rows);
+            pw.println(prop.cols);
+            pw.println(prop.delay);
+            for (int i = 0; i<prop.rows;i++){
+                for (int j = 0; j<prop.cols; j++){
+                    pw.print(prop.cells[i][j].toSerializedRep());
+                }
+                pw.println();
+            }
+            for(var pipe:prop.pipes){
+                pw.print(pipe.toSerializedRep());
+            }
+            pw.println();
+            pw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
